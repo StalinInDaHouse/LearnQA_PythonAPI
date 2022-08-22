186c5819011f1e18fa416599f2bc6d1b7124021d
d.pikalov <dmitriy.pikalov@price-team.ru>
2022-08-22 21:39:53 +0700

    .

--- Ex7.py
+++ Ex7.py
@@ -1,16 +1,16 @@
 import requests
 
-# first_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
-# print(first_response.text)
-#
-# second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "POST"})
-# print(second_response.text)
-#
-# third_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "GET"})
-# print(third_response.text)
+first_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
+print(first_response.text)
+
+second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "POST"})
+print(second_response.text)
+
+third_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "GET"})
+print(third_response.text)
 
 param = [{"j": {"method": "GET"}}, {"j": {"method": "POST"}}, {"j": {"method": "PUT"}}, {"j": {"method": "PATCH"}},
-         {"j": {"method": "DELETE"}}]
+         {"j": {"method": "DELETE"}}, {"j": {"method": "OPTIONS"}}, {"j": {"method": "HEAD"}}]
 for i in param:
     cicle_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=i["j"])
     print(f"{cicle_response.text} for {param.index(i)} in GET request")
@@ -26,3 +26,9 @@ for i in param:
 for i in param:
     cicle_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i["j"])
     print(f"{cicle_response.text} for {param.index(i)} in DELETE request")
+for i in param:
+    cicle_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i["j"])
+    print(f"{cicle_response.text} for {param.index(i)} in OPTIONS request")
+for i in param:
+    cicle_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i["j"])
+    print(f"{cicle_response.text} for {param.index(i)} in HEAD request")
