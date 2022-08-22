6002d054ecbfe6111ebc5669cb1b74b8d780c0ab
d.pikalov <dmitriy.pikalov@price-team.ru>
2022-08-22 21:29:44 +0700

    .

--- Ex7.py
+++ Ex7.py
@@ -1,6 +1,28 @@
 import requests
-import urllib3
 
-r = urllib3.PoolManager()
-response = r.request("https://playground.learnqa.ru/ajax/api/compare_query_type", {"method": 'GET'})
-print(response)
+# first_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
+# print(first_response.text)
+#
+# second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "POST"})
+# print(second_response.text)
+#
+# third_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "GET"})
+# print(third_response.text)
+
+param = [{"j": {"method": "GET"}}, {"j": {"method": "POST"}}, {"j": {"method": "PUT"}}, {"j": {"method": "PATCH"}},
+         {"j": {"method": "DELETE"}}]
+for i in param:
+    cicle_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=i["j"])
+    print(f"{cicle_response.text} for {param.index(i)} in GET request")
+for i in param:
+    cicle_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i["j"])
+    print(f"{cicle_response.text} for {param.index(i)} in POST request")
+for i in param:
+    cicle_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i["j"])
+    print(f"{cicle_response.text} for {param.index(i)} in PUT request")
+for i in param:
+    cicle_response = requests.patch("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i["j"])
+    print(f"{cicle_response.text} for {param.index(i)} in PATCH request")
+for i in param:
+    cicle_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=i["j"])
+    print(f"{cicle_response.text} for {param.index(i)} in DELETE request")
